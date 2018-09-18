ca27df26edd8ef00faf662d6e4195598b3895586
yusuke <prsprivatestock@i.softbank.jp>
2018-09-19 06:13:11 +0900

    modify .gitignore

---
+++ up_nose_rubis.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+
+cd ./backend
+docker-compose down
+docker-compose build --no-cache
+docker-compose up -d
+
+sleep 14
+cd ../nose-cli-master/nose/experiments/rubis/
+mysql --host=127.0.0.1 -uroot -proot -e 'create database rubis;' 
+mysql --host=127.0.0.1 -uroot -proot -Drubis < rubis-schema.sql
+mysql --host=127.0.0.1 -uroot -proot -Drubis < rubis-update.sql
+
+node fake.js
+
+#/home/yusuke/.rbenv/shims/bundle exec ruby /home/yusuke/Dropbox/TestNoSE/nose-cli-master/bin/nose create ./rubis_baseline
+#/home/yusuke/.rbenv/shims/bundle exec ruby /home/yusuke/Dropbox/TestNoSE/nose-cli-master/bin/nose load ./rubis_baseline
