f311a8fb4396d31d654d0bee640e0032974bef50
yusuke <prsprivatestock@i.softbank.jp>
2018-10-11 23:05:20 +0900

    mistakengly make index when table and pkey are same

--- nose-cli-master/nose/lib/nose/search/create_index.rb
+++ nose-cli-master/nose/lib/nose/search/create_index.rb
@@ -14,30 +14,28 @@ class CreateIndex
     non_key_fields > non_key_field_num_threshold
   end
 
-  #secondary indexとして置き換えるには、置き換えるクエリのselect句の中身が、実テーブルの中に含まれている必要があるのでそれを確認する
+  #yusuke secondary indexとして置き換えるには、置き換えるクエリのselect句の中身が、実テーブルの中に含まれている必要があるのでそれを確認する
   def is_column_included()
-    #TODO
+    #yusuke それぞれのindex.all_fieldsの間でSet#superset?をとる
   end
 
-  def get_MySQL_table_name_by_index(index)
-    index.hash_fields.first.to_s.split('.').first
+  def get_MySQL_primary_key_by_index(index)
+    index.hash_fields.first.to_s
   end
 
   #yusuke 同じmysql上のテーブルに言及しているindexの名前を記録しておいて返します
   def is_make_index(index)
-    mySQL_table_name = get_MySQL_table_name_by_index(index)
-    if @table_names.empty? || !@table_names.transpose[0].include?(mySQL_table_name) then
-      @table_names.append [mySQL_table_name,index.key]
+    mySQL_table_name = get_MySQL_primary_key_by_index(index).split('.').first
+    mySQL_primary_member = get_MySQL_primary_key_by_index(index).split('.').last
+    if @table_names.empty? || !@table_names.map{|tn| tn[:table_name]}.include?(mySQL_table_name) then
+      h = {table_name: mySQL_table_name,primary_mem: mySQL_primary_member,column_id: index.key}
+      @table_names.append h
       return false #yusuke 作成するcolumn familyが初めてのテーブルに対するものならindexは作成しないので返す
     end
 
     return false if !is_replace_2nd_index index #yusuke indexに置き換えることでメリットがあると判断した場合、このif文には入らない
 
-    if @pkeys.empty? ||  @pkeys.include?(mySQL_table_name) then
-      @pkeys.append(mySQL_table_name)
-      return !@table_names.select {|tn| tn[0] == mySQL_table_name}.first[1].empty? #yusuke secondary indexを貼る対象のkeyを取得する
-    end
-    false
+    return !@table_names.select {|tn| tn[:table_name] == mySQL_table_name}.empty? #yusuke secondary indexを貼る対象のkeyを取得する
   end
 
   def get_has_index_hash
