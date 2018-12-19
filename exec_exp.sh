0a01289ab2bd6fcf13428e9090ec3055f501d0ca
yusuke <prsprivatestock@i.softbank.jp>
2018-12-19 09:47:41 +0900

    #53 increase cassandra batch limit size

--- exec_exp.sh
+++ exec_exp.sh
@@ -3,7 +3,7 @@
 set -e
 
 cd ./nose-cli-master/nose/experiments/rubis/
-#mysql --host=127.0.0.1 -uroot -proot -e 'create database rubis;' 
+mysql --host=127.0.0.1 -uroot -proot -e 'create database rubis;' 
 mysql --host=127.0.0.1 -uroot -proot -Drubis < rubis-schema.sql
 mysql --host=127.0.0.1 -uroot -proot -Drubis < rubis-update.sql
 
