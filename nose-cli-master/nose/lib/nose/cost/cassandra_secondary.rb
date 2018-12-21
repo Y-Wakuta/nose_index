5be51d03d4e4227677edd250ba3a6f5ac28de38d
yusuke <prsprivatestock@i.softbank.jp>
2018-12-21 15:45:46 +0900

    #39 rename cost model

--- nose-cli-master/nose/lib/nose/cost/cassandra_secondary.rb
+++ nose-cli-master/nose/lib/nose/cost/cassandra_secondary.rb
@@ -6,15 +6,38 @@ module NoSE
     class CassandraSecondaryCost < Cost
       include Subtype
 
+      #yusuke FieldSizeCostのindex_lookup_costの中身を抜き出してきた
+      def get_size(step)
+        # If we have an answer to the query, we only need
+        # to fetch the data fields which are selected
+        fields = step.index.all_fields
+        fields &= step.state.query.select if step.state.answered?
+
+        step.state.cardinality * fields.sum_by(&:size)
+      end
+
       # Rough cost estimate as the number of requests made
       # @return [Numeric]
-      def index_lookup_cost(step)
+      # yusuke 他のcost modelも見比べると非常にhintにできる
+      # step.is_a? Plans::IndexLookupPlanStep == trueは常に成立する
+      def index_lookup_cost(step,aggregated_steps = nil)
         return nil if step.state.nil?
-        rows = step.state.cardinality
-        parts = step.state.hash_cardinality
+        rows = step.state.cardinality #yusuke RequestCountCostの実装から考えるとこれが論文のn
+        parts = step.state.hash_cardinality #yusuke これは他のcost modelでは参照されていないが、step.state.cardinalityがnであることを考えるとこれがwか？
+        client_network_cost =  @options[:client_network_cost]
+        parent = step.parent
+        #childがいるかどうかの判定はansweredで行う
+        cost = @options[:index_cost] + parts * @options[:partition_cost] + rows * @options[:row_cost]
 
-        @options[:index_cost] + parts * @options[:partition_cost] +
-          rows * @options[:row_cost]
+        if !parent.is_a? Plans::IndexLookupPlanStep #yusuke index_lookupにおいては一段階目
+          cost += client_network_cost #yusuke client -> cassandra
+          cost += client_network_cost * get_size(step) if !step.index.is_secondary_index #yusuke CF: cassandra -> client
+        else #yusuke 二段目以降
+          cost += client_network_cost if !parent.index.is_secondary_index#yusuke CF: client -> cassandra
+          cost += client_network_cost * get_size(step) #yusuke cassandra -> client
+        end
+
+        cost
       end
 
       # Cost estimate as number of entities deleted
@@ -30,4 +53,5 @@ module NoSE
       end
     end
   end
+
 end
