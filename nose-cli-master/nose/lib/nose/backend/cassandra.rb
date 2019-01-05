b4b243ff557bc644b37c1b1d9139c6102bd9d7a1
Y-Wakuta <prsprivatestock@i.softbank.jp>
2019-01-05 20:41:50 +0900

    #53-2 change secondary index -> sstable attatched secondary index

--- nose-cli-master/nose/lib/nose/backend/cassandra.rb
+++ nose-cli-master/nose/lib/nose/backend/cassandra.rb
@@ -134,7 +134,7 @@ module NoSE
       def index_cql(index,has_index_hash) #yusuke ここでplan_fileの内容からCQLを生成している
         has_index = has_index_hash.select{|has_index| has_index.index_key ==  index.key && has_index.index_value}.first
         if !has_index.nil?
-          ddl = "CREATE INDEX IF NOT EXISTS #{index.key} ON #{has_index.parent_table_id}(#{(field_names index.hash_fields).split(',').first});"
+          ddl = "CREATE CUSTOM INDEX IF NOT EXISTS #{index.key} ON #{has_index.parent_table_id}(#{(field_names index.hash_fields).split(',').first}) USING 'org.apache.cassandra.index.sasi.SASIIndex';"
           return ddl
         end
 
